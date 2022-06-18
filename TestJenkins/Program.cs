74b140483b77f2796d20904502d75c6d0acbc5bc
OdMorales <OdMorales@DESKTOP-MH402MR>
2022-06-18 16:16:20 -0500

    firts-commit

---
+++ TestJenkins/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace TestJenkins // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
